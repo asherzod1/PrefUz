1aa950ec8adb6d8c3f68385b049a6c1b4ce0e30a
sherzod <otaqulovsherzod2503@gmail.com>
2021-06-27 00:57:52 +0300

    'Osherzod'

--- src/comps/smallComps/Footerr.jsx
+++ src/comps/smallComps/Footerr.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import '...'
 const Footer = () => {
     return (
         <div>
-            
+            <div className="container">
+                <div className="footerPref">
+
+                </div>
+            </div>
         </div>
     )
 }
