17ff088f028a9e9389d740cbdca58584a1fda705
asherzod1 <sh.abdumutalov1@gmail.com>
2021-06-28 21:16:32 +0500

    Asherzod

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
-import HomeP from './pages/HomeP';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import HomeP from './pages/HomeP';
+import DoctorPage from "./pages/DoctorPage";
+import AllJobsPage from './pages/AllJobsPage';
 import './styles/style.scss'
 import './styles/FooterPref.scss'
 import './styles/doctorpage.scss'
 import './styles/HomeC.scss'
-import DoctorPage from "./pages/DoctorPage";
-<<<<<<< HEAD
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-=======
-import AllJobsPage from './pages/AllJobsPage';
-
->>>>>>> 1a87d462559fe1f8fd592f1d6193050363586047
 function App() {
 
     const routes = [
--- src/comps/allJobsPage/AllJobsCards.jsx
+++ src/comps/allJobsPage/AllJobsCards.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Link from 'react-router-dom/Link'
-import {Row, Col} from 'antd'
 import '../../styles/AllJobs.scss'
 const AllJobsCards = () => {
     return (
