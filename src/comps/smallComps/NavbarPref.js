89d4f05c82f2deede7036991e3ed24bb0cec73a8
sherzod <otaqulovsherzod2503@gmail.com>
2021-07-03 02:58:48 +0300

    'LOPEW'

--- src/comps/smallComps/NavbarPref.js
+++ src/comps/smallComps/NavbarPref.js
@@ -1,4 +1,9 @@
 import React,{useState} from 'react';
+import Link from "react-router-dom/Link";
+import {NavLink} from 'react-router-dom'
+import { Button, Input, Form, Select } from 'antd';
+import { Option } from 'antd/lib/mentions';
+import '../../styles/style.scss'
 import {
     Collapse,
     Navbar,
@@ -15,16 +20,7 @@ import {
     ModalFooter,
     Modal
 } from 'reactstrap';
-import Link from "react-router-dom/Link";
-import {NavLink} from 'react-router-dom'
-<<<<<<< HEAD
-import '../../styles/style.scss'
-=======
-import { Button, Input, Form, Select } from 'antd';
-import { Option } from 'antd/lib/mentions';
 
-
->>>>>>> b8d8227a7d8072cb839f9c6071f1b7914e04ce9b
 function NavbarPref(props) {
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
@@ -182,11 +178,7 @@ function NavbarPref(props) {
                                 <div className="navbar-end">
                                     <img src="/image/user.png" alt=""/>
                                 </div>
-<<<<<<< HEAD
-                                <DropdownToggle  nav caret>
-=======
                                 <DropdownToggle nav caret onClick={toggleModal}>
->>>>>>> b8d8227a7d8072cb839f9c6071f1b7914e04ce9b
                                     Kirish
                                 </DropdownToggle>
                                 <DropdownMenu right>
@@ -198,7 +190,6 @@ function NavbarPref(props) {
                                             pushkin12@gmail.com
                                         </div>
                                     </DropdownItem>
-                                    <DropdownItem divider />
                                     <DropdownItem>
                                         Natijalar
                                     </DropdownItem>
